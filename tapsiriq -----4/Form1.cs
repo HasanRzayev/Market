71e087dfe60251512ae7ad90ce9b05bffbdacc38
Rzay_ri48@itstep.edu.az <Rzay_ri48@itstep.edu.az>
2022-02-25 19:02:38 +0400

    yeniiiiiiiiiiiiii----

--- tapsiriq -----4/Form1.cs
+++ tapsiriq -----4/Form1.cs
@@ -12,26 +12,33 @@ using System.Windows.Forms;
 
 namespace tapsiriq______4
 {
- 
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
-         
+
 
         }
-        List<ProductUC> products = new List<ProductUC>() {};
+        List<ProductUC> products = new List<ProductUC>() { };
         List<Product> option = new List<Product>();
+        List<History> producthistory = new List<History>();
 
 
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            var hesabjson = File.ReadAllText("baza.json");
-            if(!string.IsNullOrWhiteSpace(hesabjson))
-            {
 
+            //var his = File.ReadAllText("history.json");
+            //producthistory = JsonConvert.DeserializeObject<List<History>>(his);
+
+
+            var hesabjson = File.ReadAllText("baza.json");
+            option = JsonConvert.DeserializeObject<List<Product>>(hesabjson);
+            if (option.FindAll(n=>/*(n as Product).count.ToString()=="Bitdi" |*/ (n as Product).count== 0).Count>4)
+            {
+                option.Clear();
                 option.Add(new Product("Coca-Cola", 1.5, 10, "https://cdn.iconscout.com/icon/premium/png-256-thumb/cocacola-178382.png"));
                 option.Add(new Product("Pepsi", 2.1, 10, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSOFezLgs9hqE8HLuDPYqF2Awm6U2wLUiKgA&usqp=CAU"));
                 option.Add(new Product("Fanta", (3.5), 10, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIifzWgblZmWXfi160v_3vw0gne_R54gDm7A&usqp=CAU"));
@@ -44,49 +51,51 @@ namespace tapsiriq______4
                 option.Add(new Product("Twix", 5.6, 10, "https://icon2.cleanpng.com/20180409/rue/kisspng-twix-chocolate-bar-mars-milk-candy-cookie-5acb2e3281ce00.2273096315232650745317.jpg"));
                 option.Add(new Product("M&M", 2.4, 10, "https://images.heb.com/is/image/HEBGrocery/000121396?fit=constrain,1&wid=800&hei=800&fmt=jpg&qlt=85,0&resMode=sharp2&op_usm=1.75,0.3,2,0"));
                 option.Add(new Product("Oreo", 1.6, 10, "https://digitalcontent.api.tesco.com/v2/media/ghs/e6b2e65a-cd7d-41cc-87a5-1c4108950f6b/3603d539-4f25-4273-930f-f790d22b9195_1014455455.jpeg?h=540&w=540"));
+                option.Add(new Product("Tutku", 1.6, 10, "https://www.bosphorusmarket.com/uploads/1/3/4/0/134063848/s435321757357478238_p1514_i1_w1200.jpeg"));
 
 
-                int x = 0;
-                int y = 0;
 
-                for (int i = 0; i < option.Count; i++)
-                {
-                    ProductUC lazim = new ProductUC(ref SEBET);
-                    lazim.button_name.Text = option[i].name;
-                    lazim.pictureBox1.LoadAsync($@"{option[i].image}");
-                    lazim.pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
-                    lazim.odenilmeli = label_odenilmeli;
-                    lazim.label_qiymet.Text = option[i].money.ToString();
 
-                    if (i % 3 == 0 && i != 0)
-                    {
 
-                        y += 400;
-                        x = 0;
-
-                        lazim.Location = new Point(x, y);
-                    }
-                    else if (i == 0)
-                    {
-                        lazim.Location = new Point(x, y);
-                    }
-                    else
-                    {
-
-                        x += 200;
-                        lazim.Location = new Point(x, y);
-                    }
-                    products.Add(lazim);
-                    Controls.Add(lazim);
-                }
             }
-            else
+         
+
+
+            int x = 0;
+            int y = 0;
+            for (int i = 0; i < option.Count; i++)
             {
-                products = JsonConvert.DeserializeObject<List<ProductUC>>(hesabjson);
+                ProductUC lazim = new ProductUC(ref SEBET);
+                lazim.button_name.Text = option[i].name;
+                lazim.pictureBox1.LoadAsync($@"{option[i].image}");
+                lazim.pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                lazim.button_name.Text = option[i].name;
+                lazim.label_qiymet.Text = (option[i].money).ToString();
+                lazim.label_count.Text = (option[i].count).ToString();
+                lazim.odenilmeli = label_odenilmeli;
+                lazim.label_qiymet.Text = option[i].money.ToString();
+
+                if (i % 3 == 0 && i != 0)
+                {
+
+                    y += 400;
+                    x = 0;
+
+                    lazim.Location = new Point(x, y);
+                }
+                else if (i == 0)
+                {
+                    lazim.Location = new Point(x, y);
+                }
+                else
+                {
+
+                    x += 200;
+                    lazim.Location = new Point(x, y);
+                }
+                products.Add(lazim);
+                Controls.Add(lazim);
             }
-           
-
-
 
 
 
@@ -142,12 +151,37 @@ namespace tapsiriq______4
             if (daxil > odenilmeli)
             {
                 labelqaliq.Text = (daxil - odenilmeli).ToString();
+                
+
+
+                List<Product> lazimli = new List<Product>();
+                foreach (var item in SEBET.Items)
+                {
+                    lazimli.Add(item as Product );
+                }
+                History history = new History(lazimli,daxil,daxil-odenilmeli,DateTime.Now);
+                producthistory.Add(history);
+                var hisoryproduct = JsonConvert.SerializeObject(producthistory, Newtonsoft.Json.Formatting.Indented);
+                File.WriteAllText("history.json", hisoryproduct);
+
+
+
                 SEBET.Items.Clear();
+                for (int i = 0; i < products.Count; i++)
+                {
+                    option[i].name = products[i].button_name.Text;
+                    option[i].money = double.Parse(products[i].label_qiymet.Text);
+                    if (products[i].label_count.Text == "Bitdi") option[i].count = 0;
+                    else option[i].count = int.Parse(products[i].label_count.Text);
+                }
+                var producs = JsonConvert.SerializeObject(option, Newtonsoft.Json.Formatting.Indented);
+                File.WriteAllText("baza.json", producs);
             }
             else
             {
                 labelqaliq.Text = "Pul catismir";
             }
+
         }
 
         private void textBox_money_TextChanged(object sender, EventArgs e)
@@ -173,12 +207,12 @@ namespace tapsiriq______4
 
             for (int i = 0; i < products.Count; i++)
             {
-               foreach (var item in SEBET.Items) { 
-                    
+                foreach (var item in SEBET.Items) {
+
                     number = 0;
-                    if (products[i].button_name.Text==(item as Product).name)
+                    if (products[i].button_name.Text == (item as Product).name)
                     {
-                        
+
                         number = double.Parse(products[i].label_count.Text);
                         number += (item as Product).count;
                         products[i].label_count.Text = number.ToString();
@@ -188,10 +222,10 @@ namespace tapsiriq______4
                     }
                 }
             }
-          
 
-                
-       
+
+
+
 
 
             SEBET.Items.Clear();
@@ -307,10 +341,7 @@ namespace tapsiriq______4
 
         private void Form1_MouseMove(object sender, MouseEventArgs e)
         {
-            var producs = JsonConvert.SerializeObject(products, Newtonsoft.Json.Formatting.Indented);
 
-
-            File.WriteAllText("baza.json", producs);
         }
     }
     public class Product
@@ -337,4 +368,20 @@ namespace tapsiriq______4
         public string image { get; set; }
         public override string ToString() => $"{name:10}-------{money}------{count}";
     }
+    public class History{
+       
+
+        public History(List<Product> sebethistory, double inputmoney, double returnmoney, DateTime time)
+        {
+            this.sebethistory = sebethistory;
+            this.inputmoney = inputmoney;
+            this.returnmoney = returnmoney;
+            this.time = time;
+        }
+        public List<Product> sebethistory { get; set; }
+        public double inputmoney { get; set; }
+        public double returnmoney { get; set; }
+        public DateTime time { get; set; }
+    }
+
 }
